654936a9539602f80a8c7e725c8947e9e221c15f
a2751012962 <71187843+a2751012962@users.noreply.github.com>
2025-07-12 13:32:55 +0800

    Update config.py
    hide token api

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ AUTO_CONVERT_TO_EXCEL = True
 # ==================== zsxq知识星球配置 ====================
 STAR_ID = "48418411254128"  # 星球ID
 # 请在此处填入您的真实Cookie, 例如 "zsxq_access_token=ABC..."
-COOKIE = "zsxq_access_token=7F5314F2-F2B5-4249-AA83-C2FB4EE68219_29601D756BC25DAE; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2288441414221452%22%2C%22first_id%22%3A%221978be40064f2a-06a2ccf0c267ce-19525634-1296000-1978be4006510cf%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTk3OGJlNDAwNjRmMmEtMDZhMmNjZjBjMjY3Y2UtMTk1MjU2MzQtMTI5NjAwMC0xOTc4YmU0MDA2NTEwY2YiLCIkaWRlbnRpdHlfbG9naW5faWQiOiI4ODQ0MTQxNDIyMTQ1MiJ9%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%2288441414221452%22%7D%7D"  # 认证Cookie
+COOKIE = "“ # 认证Cookie
 
 # zsxq API配置
 API_BASE_URL = "https://api.zsxq.com/v2/topics"
@@ -28,7 +28,7 @@ MAX_TOPIC_PAGES = 50 # 获取话题的最大页数，防止无限循环
 
 # ==================== OpenAI API配置 ====================
 # 请在此处填入您的真实API Key, 例如 "sk-..."
-OPENAI_API_KEY = "sk-bb80108ed8104b08bf784feec4329e64"
+OPENAI_API_KEY = ""
 # 推荐使用DeepSeek, 或者根据需要替换为 "https://api.openai.com/v1"
 OPENAI_API_BASE = "https://api.deepseek.com"
 OPENAI_MODEL = "deepseek-chat"
